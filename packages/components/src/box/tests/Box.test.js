0c8fdc7e86c1b7cd38587eee01263b2418d38040
Richie McColl <richie.mccoll@hotmail.co.uk>
2021-01-14 17:03:08 +0000

    chore: tests for inline block box

--- packages/components/src/box/tests/Box.test.js
+++ packages/components/src/box/tests/Box.test.js
@@ -2,9 +2,21 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Box from "../";
 
+const NODE_TYPES = {
+  div: 'DIV',
+  span: 'SPAN'
+};
+
 describe("Box", () => {
-  it("should render children", () => {
+  it("should render children and block by default", () => {
     const { getByText } = render(<Box>Default</Box>);
-    getByText("Default");
+    const box = getByText("Default");
+    expect(box.nodeName).toEqual(NODE_TYPES.div);
+  });
+
+  it(`should accept the 'as' prop for inline blocks`, () => {
+    const { getByText } = render(<Box as="span">Default</Box>);
+    const box = getByText("Default");
+    expect(box.nodeName).toEqual(NODE_TYPES.span);
   });
 });
