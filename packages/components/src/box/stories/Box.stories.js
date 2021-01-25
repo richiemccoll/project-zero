a56c2e388e58be0c2d591b653c17366a81c97f35
Richie McColl <richie.mccoll@hotmail.co.uk>
2021-01-25 10:41:30 +0000

    feat: add dark mode support for box component

--- packages/components/src/box/stories/Box.stories.js
+++ packages/components/src/box/stories/Box.stories.js
@@ -8,13 +8,17 @@ export default {
     argTypes: {},
 };
 
-const Template = (args) => (
+const Default = () => (
     <Box width={[1, 1 / 2]} p={4} mb={3} bg="#eee">
         Everything in web design is a box, or the absence of a box.
     </Box>
 );
 
-export const Standard = Template.bind({});
-Standard.args = {
-    label: 'Box',
-};
+const Inverted = () => (
+    <Box width={[1, 1 / 2]} p={4} mb={3} invert>
+        Everything in web design is a box, or the absence of a box.
+    </Box>
+);
+
+export const Standard = Default.bind({});
+export const Invert = Inverted.bind({});
