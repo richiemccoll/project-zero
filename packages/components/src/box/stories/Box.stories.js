cf3d39d398bfab1e3f033a08f79d04cc3fcedfba
Richie McColl <richie.mccoll@hotmail.co.uk>
2021-01-26 15:12:06 +0000

    Revert "feat: add dark mode support for box component"
    This reverts commit a56c2e388e58be0c2d591b653c17366a81c97f35.

--- packages/components/src/box/stories/Box.stories.js
+++ packages/components/src/box/stories/Box.stories.js
@@ -8,17 +8,13 @@ export default {
     argTypes: {},
 };
 
-const Default = () => (
+const Template = (args) => (
     <Box width={[1, 1 / 2]} p={4} mb={3} bg="#eee">
         Everything in web design is a box, or the absence of a box.
     </Box>
 );
 
-const Inverted = () => (
-    <Box width={[1, 1 / 2]} p={4} mb={3} invert>
-        Everything in web design is a box, or the absence of a box.
-    </Box>
-);
-
-export const Standard = Default.bind({});
-export const Invert = Inverted.bind({});
+export const Standard = Template.bind({});
+Standard.args = {
+    label: 'Box',
+};
