26e18370e1ef87b5648360d8f071b3758e57b50d
Richie McColl <richie.mccoll@hotmail.co.uk>
2021-01-27 10:04:01 +0000

    chore: run executables from closest node_modules dir

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-npx lerna exec -- npm run check-types
-npx lint-staged
+# Run executables from the closest node_modules
+function npm-do { (PATH=$(npm bin):$PATH; eval $@;) }
+
+npm-do lerna exec -- npm run check-types
+npm-do lint-staged
